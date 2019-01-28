e32e5be9e34476254be4ab7b68cf76bb0b3cf0d5
Olivier Roques <olivier.roques@telecom-paristech.fr>
2019-01-28 21:50:40 +0000

    Update private_rps

--- private_rps.sol
+++ private_rps.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.5.1;
 
 contract RockPaperScissors {
 
     uint constant public BET_MIN        = 1 finney;
     uint constant public REVEAL_TIMEOUT = 10 minutes;
-    uint public firstReveal;
+    uint private firstReveal;
 
     enum Moves {None, Rock, Paper, Scissors}
     enum Outcomes {None, PlayerA, PlayerB, Draw}
@@ -12,8 +12,8 @@ contract RockPaperScissors {
     address payable playerA;
     address payable playerB;
 
-    bytes32 private hashedMovePlayerA;
-    bytes32 private hashedMovePlayerB;
+    bytes32 private encrMovePlayerA;
+    bytes32 private encrMovePlayerB;
 
     Moves private movePlayerA;
     Moves private movePlayerB;
@@ -38,9 +38,8 @@ contract RockPaperScissors {
         } else if (playerB == address(0x0)) {
             playerB = msg.sender;
             return 2;
-        } else {
-            return 0;
         }
+        return 0;
     }
 
 
@@ -51,11 +50,11 @@ contract RockPaperScissors {
         _;
     }
 
-    function play(bytes32 hashedMove) public isRegistered returns (bool) {
-        if (msg.sender == playerA && hashedMovePlayerA == 0x0) {
-            hashedMovePlayerA = hashedMove;
-        } else if (msg.sender == playerB && hashedMovePlayerB == 0x0) {
-            hashedMovePlayerB = hashedMove;
+    function play(bytes32 encrMove) public isRegistered returns (bool) {
+        if (msg.sender == playerA && encrMovePlayerA == 0x0) {
+            encrMovePlayerA = encrMove;
+        } else if (msg.sender == playerB && encrMovePlayerB == 0x0) {
+            encrMovePlayerB = encrMove;
         } else {
             return false;
         }
@@ -66,16 +65,22 @@ contract RockPaperScissors {
     /* REVEAL PHASE */
 
     modifier commitPhaseEnded() {
-        require(hashedMovePlayerA != 0x0 && hashedMovePlayerB != 0x0);
+        require(encrMovePlayerA != 0x0 && encrMovePlayerB != 0x0);
         _;
     }
 
-    function reveal(bytes32 move) public isRegistered commitPhaseEnded returns (Moves) {
-        bytes32 hashedMove = sha256(abi.encode(move));
-        if (msg.sender == playerA && hashedMove == hashedMovePlayerA) {
-            movePlayerA = Moves(uint(uint8(move[0])));
-        } else if (msg.sender == playerB && hashedMove == hashedMovePlayerB) {
-            movePlayerB = Moves(uint(uint8(move[0])));
+    function reveal(string memory clearMove) public isRegistered commitPhaseEnded returns (Moves) {
+        bytes32 encrMove = sha256(abi.encodePacked(clearMove));
+        Moves move       = Moves(getFirstChar(clearMove));
+
+        if (move == Moves.None) {
+            return Moves.None;
+        }
+
+        if (msg.sender == playerA && encrMove == encrMovePlayerA) {
+            movePlayerA = move;
+        } else if (msg.sender == playerB && encrMove == encrMovePlayerB) {
+            movePlayerB = move;
         } else {
             return Moves.None;
         }
@@ -83,7 +88,25 @@ contract RockPaperScissors {
         if (firstReveal == 0) {
             firstReveal = now;
         }
+
+        return move;
     }
+
+    function getFirstChar(string memory str) private pure returns (uint) {
+        byte firstByte = bytes(str)[0];
+        if (firstByte == 0x31) {
+            return 1;
+        } else if (firstByte == 0x32) {
+            return 2;
+        } else if (firstByte == 0x33) {
+            return 3;
+        } else {
+            return 0;
+        }
+    }
+
+
+    /* RESULT PHASE */
 
     modifier revealPhaseEnded() {
         require((movePlayerA != Moves.None && movePlayerB != Moves.None) ||
@@ -93,6 +116,7 @@ contract RockPaperScissors {
 
     function getOutcome() public revealPhaseEnded returns (Outcomes) {
         Outcomes outcome;
+
         if (movePlayerA == movePlayerB) {
             outcome = Outcomes.Draw;
         } else if ((movePlayerA == Moves.Rock     && movePlayerB == Moves.Scissors) ||
@@ -103,13 +127,12 @@ contract RockPaperScissors {
         } else {
             outcome = Outcomes.PlayerB;
         }
+
         pay(outcome);
         reset();
+
         return outcome;
     }
-
-    
-    /* END GAME */
 
     function pay(Outcomes outcome) private {
         if (outcome == Outcomes.PlayerA) {
@@ -123,17 +146,17 @@ contract RockPaperScissors {
     }
 
     function reset() private {
-        firstReveal       = 0;
-        playerA           = address(0x0);
-        playerB           = address(0x0);
-        hashedMovePlayerA = 0x0;
-        hashedMovePlayerB = 0x0;
-        movePlayerA       = Moves.None;
-        movePlayerB       = Moves.None;
+        firstReveal     = 0;
+        playerA         = address(0x0);
+        playerB         = address(0x0);
+        encrMovePlayerA = 0x0;
+        encrMovePlayerB = 0x0;
+        movePlayerA     = Moves.None;
+        movePlayerB     = Moves.None;
     }
 
 
-    /* INFORMATION FUNCTION */
+    /* HELPER FUNCTIONS */
 
     function getContractBalance() public view returns (uint) {
         return address(this).balance;
@@ -147,5 +170,20 @@ contract RockPaperScissors {
         } else {
             return 0;
         }
+    }
+
+    function bothPlayed() public view returns (bool) {
+        return (encrMovePlayerA != 0x0 && encrMovePlayerB != 0x0);
+    }
+
+    function bothRevealed() public view returns (bool) {
+        return (movePlayerA != Moves.None && movePlayerB != Moves.None);
+    }
+
+    function revealTimeLeft() public view returns (int) {
+        if (firstReveal != 0) {
+            return int((REVEAL_TIMEOUT + firstReveal) - now);
+        }
+        return 0;
     }
 }
